c8fb136fe6434a383e70391a0eb35d9fa998ce04
idryzhakov <ildryzhakov@gmail.com>
2020-10-16 11:41:20 +0300

    added some functional

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
-let mainSelect = document.getElementById("main-select");
-let saveBtn = document.getElementById("save-btn");
-let mainInput = document.getElementById("main-input");
+const mainSelect = document.getElementById('main-select');
+const saveBtn = document.getElementById('save-btn');
+const mainInput = document.getElementById('main-input');
 
-saveBtn.addEventListener("click",function(event) {
+saveBtn.addEventListener('click',function(event) {
  
  event.preventDefault();
 
- let selectedIndex = mainSelect.options.selectedIndex;
- let val = mainInput.value;
+ const selectedIndex = mainSelect.options.selectedIndex;
+ const val = mainInput.value;
 
- if (val === "") {
+ if (!val) {
 
-  mainInput.style.borderColor = "red";
+  mainInput.style.borderColor = 'red';
      
  } else {
 
   mainSelect.options[selectedIndex].textContent = val;
-  mainInput.style.borderColor = "lightskyblue";
+  mainInput.style.borderColor = 'lightskyblue';
     
  }
+});
+
+mainSelect.addEventListener('change',function(evt) {
+   
+    const selectedIndex = mainSelect.options.selectedIndex;
+    mainInput.style.borderColor = 'lightskyblue';
+    mainInput.value = mainSelect.options[selectedIndex].textContent;
+   
 });
